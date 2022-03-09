eec707a0d9615c33788afd57e11c35691e2b0428
Rudi Schneider <schneider-rudi@protonmail.ch>
2022-03-09 16:04:11 +0100

    ...

--- Intern/RayCore/src/PathResolver.cpp
+++ Intern/RayCore/src/PathResolver.cpp
@@ -8,11 +8,14 @@
 static std::optional<std::filesystem::path> ROOT;
 
 void initPathResolver(char* executablePath) {
+    RAYX_WARN << "executablePath=" << executablePath;
     std::filesystem::path p = std::filesystem::canonical(
         executablePath);  // rayreworked/build/bin/TerminalApp
+    RAYX_WARN << "canonical=" << p;
     p = p.parent_path();  // rayreworked/build/bin/
     p = p.parent_path();  // rayreworked/build
     p = p.parent_path();  // rayreworked
+    RAYX_WARN << "root=" << p;
     ROOT = p;
 }
 
